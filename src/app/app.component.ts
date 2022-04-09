9f0705902c8e5eb7601ee2b65fa290eee601ff74
Rodrigo <rodrigo.fantibon@hotmail.com>
2022-04-09 00:15:52 -0300

    Componentes Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  curso: String
+  cargaHoraria: Number
+  escolher(){
+    this.cargaHoraria =  Math.floor(Math.random() * 6) + 1
+  }
+  // nome;
+  // idade = 20
+  // esconderCaixa = false
+  
+  // alterarNome(evento){
+  //   //console.log(nome)
+  //   console.log(evento.target.value)
+  //   this.nome = evento.target.value
+  // }
+
+  // adicionar(nomeInput){
+  //   console.log("Adicionando...")
+  //   console.log(nomeInput.value)
+  //   this.nome = nomeInput.value
+  // }
+
+}
---
+++ src/app/listar-curso/listar-curso.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'listar-curso',
+  templateUrl: './listar-curso.component.html'
+})
+export class ListarCursoComponent {
+  
+  @Input () cursoArray = []
+  
+}
